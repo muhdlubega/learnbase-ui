07cc0303cbe2c3d7ad3c21217be658c7a2445cba
lubega-deriv <lubega@deriv.com>
2024-01-13 22:58:58 +0800

    textfield component

---
+++ libs/text-field/src/lib/index.tsx
@@ -0,0 +1,143 @@
+import React, { ChangeEvent, ComponentProps, useRef, useState } from 'react';
+import { ColorVariants, SizeVariants } from '@learnbase-ui/global';
+import { textFieldStyles } from './text-field.class';
+import { twMerge } from 'tailwind-merge';
+
+export type TextFieldSizeProps = Exclude<
+  SizeVariants,
+  '2xl' | '3xl' | '4xl' | '5xl'
+>;
+
+export type WidthProps = Exclude<
+  SizeVariants,
+  'xs' | 'xl' | '2xl' | '3xl' | '4xl' | '5xl'
+>;
+
+export interface TextFieldProps extends ComponentProps<'input'> {
+  borderWidth?: TextFieldSizeProps;
+  color?: ColorVariants;
+  defaultValue?: string;
+  disabled?: boolean;
+  fullWidth?: boolean;
+  icon?: React.ReactNode;
+  iconPosition?: 'start' | 'end';
+  isInvalid?: boolean;
+  label?: string;
+  labelPosition?: 'top' | 'bottom';
+  onIconClick?: () => void;
+  rounded?: 'none' | 'sm' | 'md' | 'lg' | 'full';
+  textSize?: TextFieldSizeProps;
+  width?: WidthProps;
+}
+
+export const TextField: React.FC<TextFieldProps> = ({
+  borderWidth,
+  className,
+  color = 'primary',
+  defaultValue = '',
+  disabled,
+  fullWidth,
+  icon,
+  iconPosition,
+  isInvalid,
+  label,
+  labelPosition = 'top',
+  maxLength,
+  name = 'TextField',
+  onChange,
+  onIconClick,
+  placeholder,
+  rounded,
+  textSize,
+  width,
+  ...rest
+}) => {
+  const [value, setValue] = useState(defaultValue);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value;
+    setValue(newValue);
+    onChange?.(e);
+  };
+
+  const handleIconClick = () => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  };
+
+  return (
+    <div
+      className={twMerge(
+        textFieldStyles({
+          color,
+          rounded,
+          iconPosition,
+          fullWidth,
+          borderWidth,
+          width
+        }),
+        icon ? 'justify-between' : 'justify-center',
+        className,
+        `inline-flex center min-w-[60px] cursor-pointer ${
+          disabled ? 'bg-gray-50 border-gray-300' : `border-${color}`
+        }`
+      )}
+    >
+      <input
+        className={twMerge(
+          textFieldStyles({
+            color,
+            textSize,
+            rounded,
+            iconPosition,
+            fullWidth,
+          }),
+          `border-none focus-visible:outline-none ${
+            disabled
+              ? 'bg-gray-50 text-gray-300 placeholder:text-gray-300'
+              : `border-${color}`
+          }`
+        )}
+        disabled={disabled}
+        id={name}
+        maxLength={maxLength}
+        onChange={handleChange}
+        placeholder={placeholder}
+        ref={inputRef}
+        type='text'
+        value={value}
+        {...rest}
+      />
+      {label && (
+        <label
+          className={`absolute block max-w-[200px] overflow-hidden whitespace-nowrap overflow-ellipsis ${
+            labelPosition === 'top'
+              ? 'translate-y-[-23px]'
+              : 'translate-y-[23px]'
+          } ${
+            disabled
+              ? 'text-gray-300 bg-gradient-to-t from-gray-50 to-transparent'
+              : 'bg-white'
+          } p-0.5 text-xs`}
+          htmlFor={name}
+        >
+          {label}
+        </label>
+      )}
+      {icon && (
+        <div
+          className={twMerge(disabled ? 'text-gray-300' : '')}
+          onClick={onIconClick ?? handleIconClick}
+        >
+          {icon}
+        </div>
+      )}
+    </div>
+  );
+};
+
+TextField.displayName = 'TextField';
+
+export default TextField;
--- libs/text-field/src/lib/text-field.spec.tsx
+++ libs/text-field/src/lib/text-field.spec.tsx
@@ -1,10 +1,57 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import TextField from '.';
 
-import TextField from './text-field';
+jest.mock('.', () => {
+  return jest.fn((props) => {
+    return (
+      <div data-testid="mocked-text-field">
+        <input
+          data-testid="mocked-input"
+          value={props.value}
+          onChange={(e) => props.onChange && props.onChange(e)}
+          disabled={props.disabled}
+        />
+        <label data-testid="mocked-label">{props.label}</label>
+        <div data-testid="mocked-icon" onClick={props.onIconClick}></div>
+      </div>
+    );
+  });
+});
 
 describe('TextField', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<TextField />);
-    expect(baseElement).toBeTruthy();
+  test('renders without errors', () => {
+    render(<TextField />);
+    const textFieldElement = document.querySelector(
+      '[data-testid="mocked-text-field"]'
+    );
+
+    expect(textFieldElement).toBeInTheDocument();
+  });
+
+  test('handles input change', () => {
+    const { container } = render(<TextField />);
+    const inputElement = container.querySelector(
+      '[data-testid="mocked-input"]'
+    );
+
+    fireEvent.change(inputElement as Element, { target: { value: 'New Value' } });
+
+    expect(inputElement).toHaveValue('New Value');
+  });
+
+  test('handles icon click', () => {
+    const onIconClickMock = jest.fn();
+    const { container } = render(
+      <TextField
+        icon={<div data-testid="mocked-icon">Icon Content</div>}
+        onIconClick={onIconClickMock}
+      />
+    );
+
+    const iconElement = container.querySelector('[data-testid="mocked-icon"]');
+    fireEvent.click(iconElement as Element);
+
+    expect(onIconClickMock).toHaveBeenCalled();
   });
 });
