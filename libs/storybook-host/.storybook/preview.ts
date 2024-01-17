c6831cb61b0990f1b5d339f66a577346187af419
jia-deriv <jiawei@regentmarkets.com>
2024-01-17 22:41:31 +0800

    update

---
+++ libs/storybook-host/.storybook/preview.ts
@@ -0,0 +1,27 @@
+import { themes } from '@storybook/theming';
+
+export const parameters = {
+  darkMode: {
+    // Override the default dark theme
+    dark: { ...themes.dark, appBg: 'black' },
+    // Override the default light theme
+    light: { ...themes.normal, appBg: 'red' },
+  },
+  themes: {
+    clearable: false,
+    list: [
+      {
+        name: 'Light',
+        class: [],
+        color: '#ffffff',
+        default: true,
+      },
+      {
+        name: 'Dark',
+        // The class dark will be added to the body tag
+        class: ['dark'],
+        color: '#000000',
+      },
+    ],
+  },
+};
